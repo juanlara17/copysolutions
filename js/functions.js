821504003c00ae80960db86f945d23cf2aecde18
Juan Felipe Lara <juanlara17@hotmail.com>
2019-03-21 12:22:40 -0500

    Adicion de seccion Portafolio

--- js/functions.js
+++ js/functions.js
@@ -1,9 +1,14 @@
 function initMap() {
-    // The location of Uluru
-    var uluru = {lat: 11.016825, lng: -74.809476};
-    // The map, centered at Uluru
-    var map = new google.maps.Map(
-        document.getElementById('map'), {zoom: 4, center: uluru});
-    // The marker, positioned at Uluru
-    var marker = new google.maps.Marker({position: uluru, map: map});
+    var myLatLng = {lat: 11.016825, lng: -74.809476};
+
+    var map = new google.maps.Map(document.getElementById('map'), {
+      zoom: 12,
+      center: myLatLng
+    });
+
+    var marker = new google.maps.Marker({
+      position: myLatLng,
+      map: map,
+      title: 'Hello World!'
+    });
   }
